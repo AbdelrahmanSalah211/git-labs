94c8380b705d512db62571255713cfc81b28b5d0
Islam-Tarek <islamtarek258@gmail.com>
2025-03-04 11:57:04 +0200

    edit function.js file

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
-function printMyName(){
+function printMyName() {
   console.log("My Name is Abdelrahman Salah");
+}
+
+function printMyName() {
+  console.log("HELLO! It's me. My Name is 'Islam Tarek'");
 }
