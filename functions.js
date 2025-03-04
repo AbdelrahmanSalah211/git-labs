952fced7a0863f91b6e07d3e4155aa589bc4a718
Ahmed Abdel-Nasser <ahmedabdulnasser.ai@gmail.com>
2025-03-04 12:06:59 +0200

    Improve the printMyName method.

--- functions.js
+++ functions.js
@@ -1,3 +1,16 @@
-function printMyName(){
-  console.log("My Name is Abdelrahman Salah");
+class Printer {
+  constructor(printableItem, ...otherItems) {
+    this.printableItem = printableItem;
+    this.otherItems = otherItems;
+  }
+  print() {
+    console.log(`${this.printableItem} ${this.otherItems.join(" ")}`);
+  }
 }
+function printMyName3(name, ...restOfTheName) {
+  let printer = new Printer(name, ...restOfTheName);
+  printer.print();
+}
+
+printMyName3("Ahmed", "Abdelnasser", "Abouzaid");
+printMyName3("Abdelrahman");
